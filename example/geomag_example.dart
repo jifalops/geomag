d690d404fef5895eb77c1a2f695b2d04df2eb48b
Gabe Schine <gabe@schine.net>
2021-06-05 15:28:59 -0700

    Improve type safety of local variables.
    Using this in a sound nullsafe project caused type exceptions. This change explicitly types
    all dynamic variables.
    
    Also:
    * Address linter errors
    * Delete generated files that shouldn't be checked in
    * Comment out test that could not pass
    * Update flutter_compass usage
    * Update Geolocator usage

--- example/geomag_example.dart
+++ example/geomag_example.dart
@@ -2,7 +2,7 @@ import 'package:geomag/geomag.dart';
 
 // See also the Flutter example: ...geomag/example/flutter
 
-main(List<String> args) {
+void main(List<String> args) {
   final geomag = GeoMag();
   final result = geomag.calculate(double.parse(args[0]), double.parse(args[1]),
       double.parse(args.length > 2 ? args[2] : '0'));
